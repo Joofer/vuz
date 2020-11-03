e1e490993fdf3f64e16cd241bb56296e4959f932
Joofer <joofer.sp@ya.ru>
2020-11-03 14:33:11 +0300

    Add files via upload

---
+++ Python 1/4/Matrix4.py
@@ -0,0 +1,13 @@
+M = int(input())
+N = int(input())
+A = []
+MAT = []
+
+for i in range(0, N):
+    A.append(int(input()))
+    
+for i in range(0, M):
+    MAT.append(A)
+    
+for i in range(0, M):
+    print(MAT[i])
